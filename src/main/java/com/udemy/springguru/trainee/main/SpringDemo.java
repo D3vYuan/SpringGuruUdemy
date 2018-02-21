8bc3a2257dc834f607aaf864eead08754eb78175
Yuanity <yjpuah1208@gmail.com>
2018-02-21 23:54:31 +0800

    New_Project

---
+++ src/main/java/com/udemy/springguru/trainee/main/SpringDemo.java
@@ -0,0 +1,12 @@
+package com.udemy.springguru.trainee.main;
+
+import org.springframework.boot.SpringApplication;
+
+import com.xxx.springrest.RestControlllerApp;
+
+@SpringBootApplication
+public class SpringDemo {
+	public static void main(String [] args) {
+		SpringApplication.run(SpringDemo.class, args);
+	}
+}
